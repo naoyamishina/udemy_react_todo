6c133d662de5083a36c93249a4f736337bc47aeb
mishina naoya <msnyxx811@gmail.com>
2024-01-02 20:27:16 +0900

    上限数の設定

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -1,9 +1,9 @@
 export const InputTodo = (props) => {
-  const {todoText, onChange, onClick} = props
+  const {todoText, onChange, onClick, disabled} = props
   return (
     <div className="input-area">
-      <input placeholder="TODOを入力" value={todoText} onChange={onChange}/>
-      <button onClick={onClick}>
+      <input disabled={disabled} placeholder="TODOを入力" value={todoText} onChange={onChange}/>
+      <button disabled={disabled} onClick={onClick}>
         追加
       </button>
     </div>
--- src/todo.jsx
+++ src/todo.jsx
@@ -43,9 +43,16 @@ export const Todo = () => {
     setIncompleteTodos(newIncompleteTodos);
   };
 
+  const isMaxLimitIncompleteTodos = incompleteTodos.length >= 5;
+
   return (
     <>
-      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd}/>
+      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd} disabled={isMaxLimitIncompleteTodos} />
+      {isMaxLimitIncompleteTodos && (
+        <p style={{ color: "red" }}>
+        登録できるTODOは5個までだよ〜。消化しろ〜。
+        </p>
+      )}
       <IncompleteTodos todos={incompleteTodos} onClickComplete={onClickComplete} onClickDelete={onClickDelete}/>
       <CompleteTodos todos={completeTodos} onClickBack={onClickBack}/>
     </>
