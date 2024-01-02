1cae16df7cc4d192e86a61ade64e05b3ae38b93c
mishina naoya <msnyxx811@gmail.com>
2024-01-02 10:34:40 +0900

    useStateでモック作成

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./styles.css";
 
 export const Todo = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["todo1", "todo2"]);
+  const [completeTodos, setCompleteTodos] = useState(["todo3"]);
+
   return (
     <>
       <div className="input-area">
@@ -12,24 +16,30 @@ export const Todo = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p className="todo-item">todo</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {incompleteTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p className="todo-item">todo</p>
-              <button>戻す</button>
-            </div>
-          </li>
+        <ul>
+          {completeTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>戻す</button>
+              </div>
+            </li>
+          ))}
+        </ul>
         </ul>
       </div>
     </>
